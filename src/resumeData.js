8e2634c5002d263176c1c3f81ddf86bb9495a60a
any020 <a_nyhammer@hotmail.com>
2019-01-22 22:16:18 +0100

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,141 @@
+let resumeData = {
+  "imagebaseurl": "https://techonoeticsclub.github.io/",
+  "name": "Are Nyhammer",
+  "role": "Teknologi Entreprenør",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": "Jeg er nytutdannet i Universitet i Bergen med en Bachelorgrad i Nye Medier. Jeg liker teknologi, medier, 'gadgets', video og bilder. Utenom det er jeg opptatt av trening og fysisk aktivitet",
+  "socialLinks": [{
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/",
+      "className": "fa fa-github"
+    },
+
+  ],
+  "aboutme": "Fullført Bachelorgrad i Nye Medier.",
+  "address": "Strandgaten 222, 5004 Bergen",
+  "website": "https://www.codeclassifiers.com",
+  "education": [{
+      "UniversityName": "Universitet i Bergen",
+      "specialization": "Nye Medier",
+      "MonthOfPassing": "Desember",
+      "YearOfPassing": "2018",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "UniversityName": "Numedal Idrettsfolkehøyskole",
+      "specialization": "Fotball med høyskolefag",
+      "MonthOfPassing": "Mai",
+      "YearOfPassing": "2011",
+      "Achievements": "Trener 1 Kurs"
+    },
+    {
+      "UniversityName": "Firda VGS",
+      "specialization": "Idrettslinjen",
+      "MonthOfPassing": "Juni",
+      "YearOfPassing": "2010",
+      "Achievements": "Generell Studiekompetanse"
+    }
+  ],
+  "work": [{
+      "CompanyName": "Nokas AS",
+      "specialization": "Stasjonervekter",
+      "MonthOfLeaving": "Pågående",
+      "YearOfLeaving": "2019",
+      "Achievements": "Vekterkurs Trinn 1-3"
+    },
+    {
+      "CompanyName": "Pedagogisk VikarSentral AS",
+      "specialization": "Ringevikar",
+      "MonthOfLeaving": "Mai",
+      "YearOfLeaving": "2014",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Securitas",
+      "specialization": "BES-Vakt",
+      "MonthOfLeaving": "Feb",
+      "YearOfLeaving": "2013",
+      "Achievements": "Brannvaktkurs"
+    },
+    {
+      "CompanyName": "Posten AS",
+      "specialization": "Landeveispostbud",
+      "MonthOfLeaving": "Aug",
+      "YearOfLeaving": "2012",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "skillsDescription": "Verktøy",
+  "skills": [{
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Adobe XD"
+    },
+    {
+      "skillname": "Maya"
+    },
+    {
+      "skillname": "Python"
+    },
+    {
+      "skillname": "Final Cut Pro"
+    },
+    {
+      "skillname": "Adobe Photoshop"
+    },
+    {
+      "skillname": "Node XL"
+    }
+  ],
+  "portfolio": [{
+      "name": "Bachelor prosjekt",
+      "description": "UX og Prototype testing hos M´Labs og Greensight i Bergen",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "Pitch",
+      "description": "Pitchvideo for Bachelor",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "Dronefilm av Ulriken",
+      "description": "3D- Modellering og dronefilming",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "Sommerprosjekter",
+      "description": "Javascript",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "Webserie",
+      "description": "Webserie produksjon sammen med Film og Tv- studenter",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+
+  ],
+  "testimonials": [{
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
